59b98b48d4328617b1d2046d4070480a6859ce55
Popoaca Andrei <pandrei58@yahoo.com>
2024-02-04 14:38:02 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { ContactForm, ContactList, Filter, Section } from '../components';
+import { AppStyled } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contactList = JSON.parse(localStorage.getItem('contacts'));
+    if (contactList) this.setState({ contacts: contactList });
+  }
+  componentDidUpdate(prevStates) {
+    if (prevStates.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  createNewContact = data => {
+    const normalizeFilter = data.name.toLowerCase();
+    const stateNameArray = this.state.contacts.map(({ name }) =>
+      name.toLowerCase()
+    );
+
+    !stateNameArray.includes(normalizeFilter)
+      ? this.setState({ contacts: [...this.state.contacts, data] })
+      : alert(`${data.name} is already in contacts.`);
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevStates => ({
+      contacts: prevStates.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  onFilterChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  findContact = () => {
+    const normalizeFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const findContact = this.findContact();
+    return (
+      <AppStyled>
+        <Section title="Phonebook">
+          <ContactForm createNewContact={this.createNewContact} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter filterValue={filter} onChange={this.onFilterChange} />
+          <ContactList
+            contacts={findContact}
+            deleteContact={this.deleteContact}
+          />
+        </Section>
+      </AppStyled>
+    );
+  }
+}
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AppStyled = styled.div`
+  display: flex;
+  padding: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+`;
